d3a42682567b3e00ed9eb18a77889652b6a43f03
Jeongmin <105720658+Ahn-Jeongmin@users.noreply.github.com>
2024-08-11 10:05:22 +0900

    Update Chatgpt_interface.py

--- Chatgpt_interface.py
+++ Chatgpt_interface.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key="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"
+openai.api_key= #masked *******
 messages=[]
 
 query_format="Explain about the scene in the novel {0} when {1} says {2} and explain why this explains {1}'s MBTI which is {3} in 3 sentence in one paragraph without '\n'.".format("The Picture of Dorian Gray","Basil","Well, perhaps you are right. And now good-by, Dorian. You have been the one person in my life of whom I have been really fond. I don't suppose I shall often see you again. You don't know what it cost me to tell you all that I have told you.", "I")
